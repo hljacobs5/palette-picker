986cd42dd6b1ef0fa0a233e76f3c174d8d829204
Haley Jacobs <haleyljacobs@gmail.com>
2018-11-29 16:40:39 -0700

    Lock shows icon shows on page

--- public/js/scripts.js
+++ public/js/scripts.js
@@ -4,8 +4,6 @@ var generateButton = document.querySelector('.generate-button');
 var paletteName = document.querySelector('.palette-name');
 var rectangles = document.querySelectorAll('.rectangle')
 
-// generateButton.addEventListener('click', generateHexCode)
-
 class PalettePicker {
 	constructor() {
 		this.projects = []
@@ -13,20 +11,28 @@ class PalettePicker {
 
 	createPalette () {
 		rectangles.forEach(rectangle => {
-			console.log(rectangle)
 			var randomColor = '#'+Math.floor(Math.random()*16777215).toString(16);;
 			rectangle.setAttribute('style', `fill:${randomColor}`)
 		})
 	}
 
-// createProject = () => {
-
-// 	}
   
-// toggleLocks = () => {
+	toggleLocks(event) {
+		rectangles.forEach(rectangle => {
+			rectangle.setAttribute()
+		})
+	 }  
+	 
+	// createProject = () => {
 
-//  	}  
+	// 	}
+
+	// deleteProject() {
+
+	// }
 }
 
 const User = new PalettePicker();
-User.createPalette()
+
+generateButton.addEventListener('click', User.createPalette)
+rectangles.addEventListener('click', User.toggleLocks)
